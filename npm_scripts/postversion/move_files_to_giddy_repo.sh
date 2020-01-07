5f30f3011c481dd346ef84f75f6063641586d459
Zach Weisser <zweisser@gmail.com>
2020-01-06 20:10:19 -0500

    chore(npm script): fix move files script to move only one file to collection/*.json

--- npm_scripts/postversion/move_files_to_giddy_repo.sh
+++ npm_scripts/postversion/move_files_to_giddy_repo.sh
@@ -4,10 +4,4 @@ dest_dir=~/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools
 
 echo "Moving files to ~/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools"
 
-cp -iv /Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/src/creature/hafdon_zorq.json '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools/item/hafdon; zorq.json'
-
-cp -iv /Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/src/item/hafdon_zorq.json '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools/creature/hafdon; zorq.json'
-
-cp -iv /Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/src/deity/hafdon_zorq.json '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools/deity/hafdon; zorq.json'
-
-cp -iv /Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/src/spell/hafdon_zorq.json '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools/spell/hafdon; zorq.json'
+cp -iv /Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/build/hafdon_zorq.json '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools/collection/hafdon; zorq.json'
