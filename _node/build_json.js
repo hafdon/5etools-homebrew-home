488a7b9e6a908593ad1d54779b2a55130ccf691e
Zach Weisser <zweisser@gmail.com>
2020-01-10 11:15:29 -0500

    feat(scripts): add scripts for building nested folders

--- _node/build_json.js
+++ _node/build_json.js
@@ -14,11 +14,7 @@ const elog = log.extend('error');
 
 log('starting');
 
-const buildObj = {};
-const write_location = `./build/collection/hafdon_zorq.json`;
 const FILTERSTRING = '__';
-
-let folders = [];
 
 /**
  * Get a list of the folders in the src/ director
@@ -34,11 +30,15 @@ let folders = [];
  *  to accord with GiddyLimit style )
  */
 
-const parent_dir = './src/collection/hafdon_zorq';
+const read_dir = './src';
+const write_dir = './build';
+let giddy_parent_folders = [],
+    file_array = [];
 
+// Get Giddy parent-level folders
 try {
-    folders = fs
-        .readdirSync(parent_dir)
+    giddy_parent_folders = fs
+        .readdirSync(read_dir)
         .filter(e => !e.startsWith(FILTERSTRING))
         .sort((a, b) => a.localeCompare(b));
 } catch (e) {
@@ -46,8 +46,29 @@ try {
     process.exitCode = 1;
 }
 
-log({ folders });
+log({ giddy_parent_folders });
 
+// get what will be the file names in the giddy parent-level folders
+// and whta are our second-level folders
+// inside the giddy parent folders
+
+try {
+    file_array = giddy_parent_folders.reduce((prev, curr) => {
+        let sub_dirs = fs
+            .readdirSync(`${read_dir}/${curr}`)
+            .filter(e => !e.startsWith(FILTERSTRING));
+        sub_dirs.forEach(s => {
+            prev.push(`${curr}/${s}`);
+        });
+
+        return prev;
+    }, []);
+} catch (e) {
+    elog(e);
+    process.exitCode = 1;
+}
+
+console.log({ file_array });
 /**
  * For each folder, get a list of contained files.
  * For each file, add its contents to an array
@@ -56,59 +77,74 @@ log({ folders });
  * Add that (key : string) : (value : Array{})
  * to our JSON object
  */
-folders.forEach(folder => {
+file_array.forEach(folder => {
     try {
-        buildObj[folder] = fs
-            .readdirSync(`${parent_dir}/${folder}`)
-            .filter(e => !e.startsWith(FILTERSTRING))
-            .reduce((prev, e) => {
-                let data = null;
-                log(e);
-                if (e.endsWith('.json')) {
-                    data = fs.readJSONSync(`${parent_dir}/${folder}/${e}`);
-                } else if (e.endsWith('.js')) {
-                    try {
-                        // have to back out a director because node > module.paths shows
-                        // only node_modules as a end path
-                        data = require(`../${parent_dir}/${folder}/${e}`);
-                    } catch (e) {
-                        elog({ 'Error trying to import js module': e });
-                    }
-                }
-                // if (data && folder != '_meta') {
-                //     data.uniqueId = crypto
-                //         .createHash('sha256')
-                //         .update(JSON.stringify(data))
-                //         .digest('hex');
-                // }
-                if (Object.getOwnPropertyNames(data).includes('uniqueId')) {
-                    delete data.uniqueId;
-                }
+        let buildObj = {};
 
-                prev.push(data);
-                return prev;
-            }, []);
+        let folder_array = fs
+            .readdirSync(`${read_dir}/${folder}`)
+            .filter(e => !e.startsWith(FILTERSTRING))
+            .sort((a, b) => a.localeCompare(b));
+
+        console.log({ folder_array });
+
+        buildObj = folder_array.reduce((prev, curr) => {
+            console.log({
+                folder,
+                read_dir,
+                curr,
+                prev,
+                all: `${read_dir}/${folder}/${curr}`,
+            });
+            prev[curr] = fs
+                .readdirSync(`${read_dir}/${folder}/${curr}`)
+                .filter(e => !e.startsWith(FILTERSTRING))
+                .reduce((prev, e) => {
+                    let data = null;
+                    log({ e });
+                    if (e.endsWith('.json')) {
+                        data = fs.readJSONSync(
+                            `${read_dir}/${folder}/${curr}/${e}`
+                        );
+                    } else if (e.endsWith('.js')) {
+                        try {
+                            // have to back out a director because node > module.paths shows
+                            // only node_modules as a end path
+                            data = require(`../${read_dir}/${folder}/${curr}/${e}`);
+                        } catch (e) {
+                            elog({ 'Error trying to import js module': e });
+                        }
+                    }
+                    // if (data && folder != '_meta') {
+                    //     data.uniqueId = crypto
+                    //         .createHash('sha256')
+                    //         .update(JSON.stringify(data))
+                    //         .digest('hex');
+                    // }
+                    if (Object.getOwnPropertyNames(data).includes('uniqueId')) {
+                        delete data.uniqueId;
+                    }
+
+                    prev.push(data);
+                    return prev;
+                }, []);
+            return prev;
+        }, {});
+
+        log(Object.keys(buildObj));
+        /**
+         * Write the file to the build directory
+         */
+        const write_location = `${write_dir}/${folder}.json`;
+        fs.writeJSONSync(write_location, {
+            ...buildObj,
+        });
+        log(`written to ${write_location}`);
     } catch (e) {
         elog(`Unable to create build JSON object for folder ${folder}`);
         elog(e);
         process.exitCode = 1;
     }
 });
-
-log(Object.keys(buildObj));
-
-/**
- * Write the file to the build directory
- */
-try {
-    fs.writeJSONSync(`${write_location}`, {
-        ...buildObj,
-    });
-    log(`written to ${write_location}`);
-} catch (e) {
-    elog('Unable to write file.');
-    elog(e);
-    process.exitCode = 1;
-}
 
 process.exitCode = 0;
--- _node/build_move.sh
+++ _node/build_move.sh
@@ -5,3 +5,7 @@ dest_dir=~/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools
 echo "Moving files to ~/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools"
 
 cp -iv /Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/build/hafdon_zorq.json '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/giddy/collection/hafdon; zorq.json'
+
+cp -iv /Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/build/collection/hafdon_zorq.json '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/giddy/collection/hafdon; zorq.json'
+
+cp -iv '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/5etools_pc/build/collection/Dan Dillon_Deep Magic 03 Void Magic.json' '/Users/starkat/rpg/dnd5e/_Marshbout/_home/homebrew_json/giddy/collection/Dan Dillon; Deep Magic 03 Void Magic.json'
