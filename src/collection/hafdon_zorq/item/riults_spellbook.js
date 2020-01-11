eab2ba81320ab9a0480056adc01dd7456e1f0db1
Zach Weisser <zweisser@gmail.com>
2020-01-10 20:23:39 -0500

    build

--- src/collection/hafdon_zorq/item/riults_spellbook.js
+++ src/collection/hafdon_zorq/item/riults_spellbook.js
@@ -6,17 +6,10 @@ module.exports = {
     source: 'zorq',
     entries: [
         "This spellbook, along with anything written on its pages, can't be damaged by fire or immersion in water. In addition, the spellbook doesn't deteriorate with age.",
+        'This spellbook contains the following spells:',
         {
-            type: 'spellcasting',
-            name: 'Spells',
-            headerEntries: ['This spellbook contains the following spells:'],
-            spells: {
-                '1': {
-                    slots: 4,
-                    spells: ['{@spell identify}', '{@spell ray of sickness}'],
-                },
-            },
-            footerEntries: [],
+            type: 'list',
+            items: ['{@spell identify}', '{@spell ray of sickness}'],
         },
     ],
 };
