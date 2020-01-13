eff8ae77c31dd1a088e9fb45ebb36fda3277838d
Zach Weisser <zweisser@gmail.com>
2020-01-12 21:37:17 -0500

    cleanup

--- src/collection/hafdon_zorq/monster/__(earth wave surfer).js
+++ src/collection/hafdon_zorq/monster/__(earth wave surfer).js
@@ -1,1 +1,9 @@
+/**
+ * “for the record, fez looks confident” 
+“i ride the rippling (earth) waves; some of them i ride, some push me back”
+[x] relate everything about fez to the sea
+
+tie in to Nautilus shell (holy symbol) lines, and Ulimara's symbols
+ */
+
 module.exports = {};
