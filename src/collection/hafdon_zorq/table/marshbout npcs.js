432d0a3a5335bddcffc06e05f62cd399b220a0bc
Zach Weisser <zweisser@gmail.com>
2020-01-15 20:47:53 -0500

    add some traits

--- src/collection/hafdon_zorq/table/marshbout npcs.js
+++ src/collection/hafdon_zorq/table/marshbout npcs.js
@@ -8,7 +8,7 @@ module.exports = {
     //     'Ideally, this table should be consulted in advance of a situation which would require it, as the results can feed into greater plot hooks.',
     // ],
     caption: 'Marshbout NPCs',
-    colLabels: ['1d6', 'Name', 'Description'],
+    colLabels: ['1d10', 'Name', 'Description'],
     colStyles: ['col-1 text-center', 'col-3', 'col-8'],
     rows: [
         ['1', "Nancy O'Pug", 'n/a'],
@@ -19,15 +19,11 @@ module.exports = {
             'One of the sisters guarding {@creature Soul Rock|zorq}',
         ],
         ['4', 'Igno Red McGerf', 'A stupid or oblivious dragon.'],
-        [
-            '5',
-            'A trail of blood leading to an unconscious and badly wounded member of the group responsible',
-            'n/a',
-        ],
-        [
-            '6',
-            'A living witness, who was unable or unwilling to leave the crime scene',
-            'n/a',
-        ],
+        ['5', 'Cackleburt', 'n/a'],
+        ['6', 'Bog Frillan', 'n/a'],
+        ['7', 'Bog Frillan', 'n/a'],
+        ['8', 'Bog Frillan', 'n/a'],
+        ['9', 'Bog Frillan', 'n/a'],
+        ['10', 'Bog Frillan', 'n/a'],
     ],
 };
