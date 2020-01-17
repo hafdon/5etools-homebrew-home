dca3f9a8ab107089a0fc5d082047b7b763da5fa9
Zach Weisser <zweisser@gmail.com>
2020-01-17 16:54:54 -0500

    work

---
+++ src/collection/hafdon_zorq/$title/geodetor.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'geodetor',
+    type: 'title',
+    source: 'zorq',
+
+    entries: [
+        'geodetic / geodesic: related to land surveying ',
+        'which is given out by Wehmseiper (Vemsyper) -- not vampires! , some of the foremost aauthorities on geography and geolocacy',
+    ],
+};
